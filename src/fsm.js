cd253fece354e9d8b024ef2c502df6da9fdb02fb
maximkaby <drupalmasterpro@mail.ru>
2017-09-22 12:56:07 +0300

    rty

--- src/fsm.js
+++ src/fsm.js
@@ -3,30 +3,68 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+        if(config == undefined){
+            throws ('config isn\'t passed');
+        }
+        this.states = config.states;
+        this.activeState = config.initial;
+        this.initial = config.initial;
+        this.prevState = false;
+        this.triggerSuccess = false;
+        this.redoCall = false;
+        this.history={
+            data : [this.initial],
+            index : 0
+        }
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+        //console.log(this.activeState);
+        return this.activeState;
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+        if(!this.states.hasOwnProperty(state)){
+            throws('state isn\'t exist');
+        }
+
+        this.activeState = state;
+
+        this.history.data.push(this.activeState);
+        this.history.index ++;
+        this.redoCall = false;
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+        let state = this.states[this.activeState].transitions[event];
+        if(state == undefined){
+            this.triggerSuccess = false;
+            throws('event in current state isn\'t exist');
+        }
+        this.triggerSuccess = true;
+        this.changeState(state);
+
+    }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+        this.activeState = this.initial;
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -34,26 +72,80 @@ class FSM {
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
+    getStates(event) {
+
+        let arr = [];
+
+        if(event == undefined){
+            for(let key in this.states){
+                arr.push(key);
+            }
+            return arr;
+        }
+
+        for(let key in this.states){
+            if(this.states[key].transitions
+                    .hasOwnProperty(event)){
+                arr.push(key);
+            }
+        }
+        return arr;
+        //return Object.keys(this.states).toArray();
+    }
 
     /**
      * Goes back to previous state.
      * Returns false if undo is not available.
      * @returns {Boolean}
      */
-    undo() {}
+    undo() {
+        this.history.index--;
+        if(this.history.data[this.history.index] == undefined)
+            return false;
+
+        this.redoCall = true;
+        //console.log(this.history.data);
+        this.activeState = this.history.data[this.history.index];
+
+
+        if(this.triggerSuccess == true){
+            //console.log(true);
+            return true;
+        }
+        console.log(this.history);
+        //return this.activeState;
+    }
 
     /**
      * Goes redo to state.
      * Returns false if redo is not available.
      * @returns {Boolean}
      */
-    redo() {}
+    redo() {
+        if(this.history.data[this.history.index + 1] == undefined){
+            return false;
+        }
+
+        if(this.redoCall == false)
+            return false;
+
+        this.history.index++;
+        this.activeState = this.history.data[this.history.index];
+
+        if(this.triggerSuccess == true){
+            //console.log(true);
+            return true;
+        }
+
+    }
 
     /**
      * Clears transition history
      */
-    clearHistory() {}
+    clearHistory() {
+        this.history.data.length = 0;
+        this.history.index = -1;
+    }
 }
 
 module.exports = FSM;
---
+++ test/test.js
@@ -0,0 +1,34 @@
+const FSM = require('../src/fsm');
+
+const config = {
+    initial: 'normal',
+    states: {
+        normal: {
+            transitions: {
+                study: 'busy',
+            }
+        },
+        busy: {
+            transitions: {
+                get_tired: 'sleeping',
+                get_hungry: 'hungry',
+            }
+        },
+        hungry: {
+            transitions: {
+                eat: 'normal'
+            },
+        },
+        sleeping: {
+            transitions: {
+                get_hungry: 'hungry',
+                get_up: 'normal',
+            },
+        },
+    }
+};
+
+const student = new FSM(config);
+
+student.trigger('study');
+student.undo();
